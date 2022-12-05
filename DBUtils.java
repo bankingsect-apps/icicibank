2d81a5d9f67d50db0a7ea2c8e45620dcf1516f45
Rajesh Personal <119273208+avrajes@users.noreply.github.com>
2022-12-05 10:31:53 -0800

    Update DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -1,3 +1,4 @@
+Updating this file directly in remote repo by using github GUI and merge staging to master in remote repository
 I am updating this line in Dev branch
 This is new update
 This is the database file which contains some db logic
